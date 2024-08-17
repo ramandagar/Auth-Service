15c4ff656fe21634a2c15eb5f49187b40e3fce81
ramandagar <ramandagar479@gmail.com>
2024-08-17 12:50:02 +0530

    first commit

--- src/index.js
+++ src/index.js
@@ -8,7 +8,6 @@ const db = require('./models/index')
 
 
 const setupAndStartServer = () =>{
-
     app.use(bodyParser.json());
     app.use(bodyParser.urlencoded({ extended: true }));
     app.use('/api',apiRoutes);
